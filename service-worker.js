bb2a5d91621990d88ff26ef6a43177520e82331c
codegeass25 <angelroy.longakit@deped.gov.ph>
2026-09-07 17:16:35 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,7 @@
      service-worker update.
    ===================================================================== */
 
-const CACHE_NAME = "qlogpro-offline-v17";
+const CACHE_NAME = "qlogpro-offline-v18";
 
 const PRECACHE_URLS = [
   "./",
